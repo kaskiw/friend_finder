f474399a35f5c923a6a376d9aaabfaf192ed70c4
Andrew William Kaskiw <30780132+kaskiw@users.noreply.github.com>
2018-04-07 13:51:48 -0400

    functional friend finder app

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,12 @@
+var path = require('path');
+
+module.exports = function (app) {
+    //Routing for the html. 
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    })
+
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+}
